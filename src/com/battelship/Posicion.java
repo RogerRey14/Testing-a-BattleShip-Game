7d40d03c97e6b82926a7a2f4a29ddd8e1d3c0653
Cristian Vega <cristian.vega.sanchez.st@everis.com>
2019-11-12 17:10:45 +0100

    Full TDD Posicionar + fixed errors

--- src/com/battelship/Posicion.java
+++ src/com/battelship/Posicion.java
@@ -1,55 +1,49 @@
 package com.battelship;
 
 public class Posicion {
-	
+
 	int x;
 	int y;
 	int orientacion;
-	
+
 	public Posicion(int x, int y) {
-		this.x=x;
-		this.y=y;
+		this.x = x;
+		this.y = y;
 	}
 
 	/*
 	 * Convierte el codigo de orientación a lenguaje natural
 	 */
-	public String getOrientacionString() {		
-		switch(this.orientacion) {
-		case 0: return "Derecha";
-		case 1: return "Abajo";
-		case 2: return "Arriba";
-		case 3: return "Izquierda";
-		default: return "No tiene orientacion";
+	public String getOrientacionString() {
+		switch (this.orientacion) {
+		case 0:
+			return "Derecha";
+		case 1:
+			return "Abajo";
+		case 2:
+			return "Arriba";
+		case 3:
+			return "Izquierda";
+		default:
+			return "No tiene orientacion";
 		}
 	}
-	
+
 	/*
-	 * Sobreescritura del metodo toString para representar la información de una posición
+	 * Sobreescritura del metodo toString para representar la información de una
+	 * posición
 	 */
 	@Override
 	public String toString() {
-		return "Posicion [x=" + (x+1) + ", y=" + (y+1) + ", orientacion=" + getOrientacionString() + "]";
-	}	
-	
+		return "Posicion [x=" + (x + 1) + ", y=" + (y + 1) + ", orientacion=" + getOrientacionString() + "]";
+	}
+
 	public int getX() {
 		return x;
 	}
 
-	public void setX(int x) {
-		this.x = x;
-	}
-
 	public int getY() {
 		return y;
-	}
-
-	public void setY(int y) {
-		this.y = y;
-	}
-
-	public int getOrientacion() {
-		return orientacion;
 	}
 
 	public void setOrientacion(int orientacion) {
