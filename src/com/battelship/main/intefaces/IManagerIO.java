acaf71ec48cb2a37d14f7996cb1e141c5428e517
Cristian vega <97cvega@gmail.com>
2019-11-16 13:47:47 +0100

    Refactor Menu & Add ManagerIO

--- src/com/battelship/main/intefaces/IManagerIO.java
+++ src/com/battelship/main/intefaces/IManagerIO.java
@@ -1,5 +1,11 @@
 package com.battelship.main.intefaces;
 
 public interface IManagerIO {
-
+	
+	public int inInt();
+	public void out(String data);
+	public void out(int data);
+	public int getRandomInt();
+	
+	
 }
