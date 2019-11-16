562d9a2691752014eecdffa94e84f10d5c6b8cb5
Cristian vega <97cvega@gmail.com>
2019-11-16 13:04:10 +0100

    TDD PARTIDA STEP 1 MAKE TEST

---
+++ tests/com/battelship/test/PartidaTest.java
@@ -0,0 +1,46 @@
+package com.battelship.test;
+
+import org.junit.Test;
+
+/*
+ * Esta clase de test se encarga de las pruebas unitarias de la clase Partida
+ */
+
+public class PartidaTest {
+	
+	/*
+	 * Test comprueba que se crea una Partida correctamente
+	 */
+	@Test
+	public void testCreatePartida() {
+
+		Partida partidaTest = new Partida();
+		assertEquals("RandomIA", partidaTest.getRandomIA().getNombre());
+		assertEquals("Jugador", partidaTest.getUserPlayer().getNombre());
+		assertFalse(partidaTest.isFinalJuego());
+		assertTrue(partidaTest.isTurnoJugador());
+	}
+
+	/*
+	 * Test comprueba que se empieza la partida
+	 */
+	@Test
+	public void testEmpezarPartida() {
+
+		Partida partidaTest = new Partida();
+		assertEquals(partidaTest.empezarPartida(),"Partida Empezada!");
+
+	}
+
+	/*
+	 * Test comprueba quien es el ganador
+	 */
+	@Test
+	public void testMostrarGanador() {
+
+		Partida partidaTest = new Partida();
+		assertEquals("Jugador", partidaTest.mostrarGanador());
+
+	}
+
+}
