7193e52ccbc2387a91bf44679741b591fc3473a6
Cristian vega <97cvega@gmail.com>
2019-11-16 12:43:44 +0100

    Start Again - Rebase Empty Project Version

--- tests/com/battelship/test/Game.java
+++
@@ -1,5 +0,0 @@
-package com.battelship.test;
-
-public class Game {
-
-}
--- tests/com/battelship/test/MenuTest.java
+++ tests/com/battelship/test/MenuTest.java
@@ -1,6 +1,7 @@
-package com.battelship.test;
 
 import org.junit.Test;
+
+import com.battelship.main.Menu;
 
 /*
  * Test que inicializa el juego y llama a la funcion main
